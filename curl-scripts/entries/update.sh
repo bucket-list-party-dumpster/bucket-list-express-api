b57f10096c394077d12b98cb8af321be050bd4f1
aemarquina <aeamarquina@gmail.com>
2020-04-21 15:20:36 -0400

    Updated curl scripts
    Updated Update.sh to have OWNER_ID
    
    Co-authored-by: Liz Fox <ecfox2008@gmail.com>
    Co-authored-by: Mike Tripp <mctripp94@gmail.com>
    Co-authored-by: Thanh Nguyen <tnansci@gmail.com>

--- curl-scripts/entries/update.sh
+++ curl-scripts/entries/update.sh
@@ -12,7 +12,7 @@ curl "${API}${URL_PATH}/${ID}" \
     "entry": {
       "title": "'"${TITLE}"'",
       "description": "'"${DESCRIPTION}"'",
-      "owner": "'"${ID}"'"
+      "owner": "'"${OWNERID}"'"
     }
   }'
 
